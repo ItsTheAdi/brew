87d4af7c5396a362547980ab80de1937bc5c0203
ItsTheAdi <adizeus0@gmail.com>
2024-12-20 15:57:56 +0530

    Added Cards

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,9 +1,13 @@
+import { Card } from '@material-tailwind/react'
 import React from 'react'
 
 function products() {
   return (
     <div>
         <h1>Our Curated Products</h1>
+        <Card>
+          Big Coffee Beans
+        </Card>
     </div>
   )
 }
